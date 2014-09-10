985efd1399a82cb6ae0fceb402c8ee0966b3ea39
Nicolas Barry <nicolas@stellar.org>
2014-09-10 11:00:51 -0700

    Add some tests for inflation.

--- test/testconfig.js
+++ test/testconfig.js
@@ -48,56 +48,56 @@ exports.accounts = {
   },
 
   // filler
-    "0": {
+    "A0": {
         'account' : "gaLBK8GoXJJJ5cpsUNj27x4HYjCJYCKTfH",
         'secret' : "0",
     },
-    "1": {
+    "A1": {
         'account' : "gG2xcNhjGtm6LKKei9DN9EonCgoVDMeLf6",
         'secret' : "1",
     },
-    "2": {
-        'account' : "gPy8pR73VjAjFgCskvpehZHx5MevDrVLhn",
+    "A2": {
+    	'account': "gpvwqBkBrzpd8fgBfm5Yc91W2RXzBoCpPu",
         'secret' : "2",
     },
-    "3": {
-        'account' : "g4GKCMcuhvsv2cqWVdAu3jKecD8RnHB2gX",
+    "A3": {
+    	'account': "gsLvPQX6safYaMJ4n4tUsXehdJsksN2Y23",
         'secret' : "3",
     },
-    "4": {
-        'account' : "gPgVdP2mnhMV5cHsiqGnxBtPWgHaYbcVYQ",
+    "A4": {
+    	'account': "gHJi4gCC13A18RpJqmPoDkz3uSgtBZ1XAH",
         'secret' : "4",
     },
-    "5": {
-        'account' : "gGTjjgv7HhppzkhvjsFTDGMDnU514NGuV7",
+    "A5": {
+    	'account': "gMThcQruZ2j9NZW7E4j9HofBSt4UPFHDnb",
         'secret' : "5",
     },
-    "6": {
-        'account' : "gGpmLKTSmQWqpvoaFCFzAV6Zd15tdkkUd2",
+    "A6": {
+    	'account': "gsp5it8AXeqsCjLsW5bz9Wyu6J7cjA8mQG",
         'secret' : "6",
     },
-    "7": {
-        'account' : "gnQNBMbLN2oaWTHtJ73tw3eYdr6SdW3D4e",
+    "A7": {
+    	'account': "gMSYMsNC8XrfJV1QXy1nyJmTP463o4KwX1",
         'secret' : "7",
     },
-    "8": {
-        'account' : "g9C98SHdiAuyDHebZEKJfbfxaGX4ssPZww",
+    "A8": {
+    	'account': "gaUvFtfA1QMcdxK2myCkMRv3PWacWPEBNR",
         'secret' : "8",
     },
-    "9": {
-        'account' : "gp847ow9WcPmnNpVHMQV5A4BF6vaL9Abm6",
+    "A9": {
+    	'account': "gsQ8xUVm1MZ6NvoVQAFPVwx3GEzqfBqipy",
         'secret' : "9",
     },
-    "10": {
-        'account' : "gNkzRXNwBgHvYMjoqtsuuJejm9bWTDWTS3",
+    "A10": {
+    	'account': "g3FDnCCRUbtNp2wzntBCGw3wqvzgghEVNs",
         'secret' : "10",
     },
-    "11": {
-        'account' : "gjBMVybYg1QktasM1oNQAxCcF6hVfFKf7",
+    "A11": {
+    	'account': "gKo29xyJoCe3LKv8pFsKcmdsUvq1deFZ2J",
         'secret' : "11",
     },
-    "12": {
-        'account' : "gNGQ3mzSApejLA1nzLyedJt9Vu6i8NUnzQ",
+    "A12": {
+    	'account': "gMcoxTKY6VicXWHbee2vRnqHiFPyoP8qwA",
         'secret' : "12",
     },
 
