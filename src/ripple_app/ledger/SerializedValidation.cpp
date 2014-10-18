b5ca9ec3b5521d278c45c27994db24ff03aca626
Graydon Hoare <graydon@stellar.org>
2014-10-17 17:00:30 -0700

    Remove dead code for checking non-canonical signatures; they are prohibited.

--- src/ripple_app/ledger/SerializedValidation.cpp
+++ src/ripple_app/ledger/SerializedValidation.cpp
@@ -96,8 +96,6 @@ bool SerializedValidation::isValid (uint256 const& signingHash) const
 {
     try
     {
-        const ECDSA fullyCanonical = getFlags () & vfFullyCanonicalSig ?
-                                            ECDSA::strict : ECDSA::not_strict;
         RippleAddress   raPublicKey = RippleAddress::createNodePublic (getFieldVL (sfSigningPubKey));
         return raPublicKey.isValid () &&
 			raPublicKey.verifySignature(signingHash, getFieldVL(sfSignature));
