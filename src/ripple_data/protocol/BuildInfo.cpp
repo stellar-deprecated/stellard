cf0fc483c174a5b074360fdb6d9c2b8263dec13f
MonsieurNicolas <nicolas@stellar.org>
2014-11-20 15:28:21 -0800

    rev to 0.27.1

--- src/ripple_data/protocol/BuildInfo.cpp
+++ src/ripple_data/protocol/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* BuildInfo::getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.27.0"
+        "0.27.1"
     //
     //  Must follow the format described here:
     //
